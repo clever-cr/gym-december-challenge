46afe3e66d0858f274d86d3fb183cc72fd62c47b
clever-cr <cleverumuhuza@gmail.com>
2022-12-19 18:51:26 +0200

    updates

--- components/sections/MeetUp.jsx
+++ components/sections/MeetUp.jsx
@@ -4,17 +4,27 @@ import Image from "next/image";
 const MeetUp = () => {
   return (
     <div>
-      <Image width={784} height={20} alt="img" src="/images/meet.webp"></Image>
-      <div className="bg-lightblue text-white">
-        <h1>What drives us: President Thomas F. Hofmann about TUM</h1>
-        <p>
+      <Image
+        width={784}
+        height={20}
+        alt="img"
+        src="/images/meet.webp"
+        className="relative"
+      ></Image>
+      <div className="bg-lightblue flex flex-col w-[688px] gap-y-5 px-10 py-10 absolute z-10  right-0 ">
+        <h1 className="text-[28px] font-bold text-white ">
+          What drives us: President Thomas F. Hofmann about TUM
+        </h1>
+        <p className="text-base text-white ">
           Research and innovations for society, innovative teaching and
           continuing education: This is how we shape a sustainable future,
           relying on the intelligence of the entire university and working
           closely with global partners to do so.
         </p>
         <Button
-          style={"bg-white border border-lightblue text-lightblue"}
+          style={
+            "bg-white border border-lightblue text-lightblue hover:bg-darkblue hover:text-white"
+          }
           text={"Read More"}
         />
       </div>
